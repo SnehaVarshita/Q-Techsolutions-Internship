550785e74afd6dbe0e3f3ae55b534ac462ea718c
Manyapu Sneha Varshita <138682924+SnehaVarshita@users.noreply.github.com>
2025-03-12 12:14:43 +0530

    Add files via upload

---
+++ CharFrequency.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class CharFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        sc.close();
+
+        HashMap<Character, Integer> freqMap = new HashMap<>();
+
+        for (char c : str.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+
+        System.out.println("Character frequencies:");
+        for (char c : freqMap.keySet()) {
+            System.out.println(c + ": " + freqMap.get(c));
+        }
+    }
+}
